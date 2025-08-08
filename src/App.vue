127ebe2ce044b4f1ca497de7322c44a0903adbee
Hilmee <hilmee4551@gmail.com>
2025-08-08 14:51:50 +0700

    แก้ไขหน้า App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- ส่วน Template ใช้สำหรับเขียนโครงสร้าง HTML ของหน้าจอ -->
+  <div id="app">
+    <!-- แสดงข้อความจากตัวแปร message ด้วย data binding {{ }} -->
+    <h1>{{ message}}</h1>
+    <!-- ปุ่มที่มี event @click ซึ่งจะเรียกฟังก์ชัน changeMessage เมื่อคลิก -->
+    <button @click="changeMessage">คลิกฉัน!</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// ส่วน Script สำหรับใส่ logic และจัดการข้อมูลของ component
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+// ส่วน Script สำหรับใส่ logic และจัดการข้อมูลของ component
+  name: "App",
+// data() เป็นฟังก์ชันที่คืนค่า object ที่เก็บตัวแปรของเราไว้
+data() {
+  return {
+
+    message: 'สวัสดีครับ ผมชื่อฮิลมี'
   }
+},
+
+// methods คือกลุ่มของฟังก์ชันที่เราสร้างขึ้นเพื่อจัดการ event ต่างๆ
+methods: {
+  // ฟังก์ชันนี้จะถูกเรียกเมื่อผู้ใช้คลิกปุ่ม
+ changeMessage() {
+  this.message = 'ขอบคุณที่คลิก!'
+ }
+
+},
+
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ba0bff;
+  
+}
+
+button {
+  border-radius: 200px;
+  color: white;
+  background-color: rgb(119, 0, 255);
+  width: 100px;
+  height: 50px;
+}
+
+button:hover {
+  background-color: blue;
 }
 </style>
